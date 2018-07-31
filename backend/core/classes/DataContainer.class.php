e828c3896d3b3832b6ee95d16db02956efd01168
Chris Rasys <chris.rasys@gmail.com>
2018-07-31 03:47:08 -0400

    Moving stuff around

--- backend/core/classes/DataContainer.class.php
+++ backend/core/classes/DataContainer.class.php
@@ -1,55 +1,5 @@
 <?php
 
-class Datum {
-    protected $type = null;
-    protected $value;
-
-    public function __construct($value = null) {
-        return $this($value);
-    }
-
-    public function __invoke($value = null) {
-        if (!is_null($value)) {
-            $this->storeValue($value);
-        }
-
-        return $this->value;
-    }
-
-    protected function storeValue($value) {
-        if (is_array($value)) {
-            $this->value = new DataContainer($value);
-        } else {
-            if ($this->validateByType($value)) {
-                $this->type = gettype($value);
-                $this->value = $value;
-            } // Throw stuff here?
-        }
-    }
-
-    protected function validateByType($value) {
-        $validTypes = ['bool', 'integer', 'string', 'double'];
-
-        $map = [
-            'boolean' => 'bool'
-        ];
-
-        $type = $map[$this->type] ?? $this->type;
-
-        if (in_array($type, $validTypes)) {
-            return ('is_'.$type)($value);
-        }
-
-        return true;
-    }
-
-    public function setType(String $type): String {
-        $this->type = $type;
-
-        return $this->type;
-    }
-}
-
 class DataContainer {
     protected $dataContainer = [];
 
@@ -81,7 +31,7 @@ class DataContainer {
 
     public function __set(string $name, $value) {
         if ($this->datumExists($name)) {
-            return $this->dataContainer[$name]($value);
+            $this->dataContainer[$name]($value);
         } else {
             $this->dataContainer[$name] = new Datum($value);
         }
---
+++ backend/core/classes/Datum.class.php
@@ -0,0 +1,51 @@
+<?php
+
+class Datum {
+    protected $type = null;
+    protected $value;
+
+    public function __construct($value = null) {
+        return $this($value);
+    }
+
+    public function __invoke($value = null) {
+        if (!is_null($value)) {
+            $this->storeValue($value);
+        }
+
+        return $this->value;
+    }
+
+    protected function storeValue($value) {
+        if (is_array($value)) {
+            $this->value = new DataContainer($value);
+        } else {
+            if ($this->validateByType($value)) {
+                $this->type = gettype($value);
+                $this->value = $value;
+            } // Throw stuff here?
+        }
+    }
+
+    protected function validateByType($value) {
+        $validTypes = ['bool', 'integer', 'string', 'double'];
+
+        $map = [
+            'boolean' => 'bool'
+        ];
+
+        $type = $map[$this->type] ?? $this->type;
+
+        if (in_array($type, $validTypes)) {
+            return ('is_'.$type)($value);
+        }
+
+        return true;
+    }
+
+    public function setType(String $type): String {
+        $this->type = $type;
+
+        return $this->type;
+    }
+}
