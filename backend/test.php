092b22a8902a5be3b6446af25120956f21170f9e
Chris Rasys <chris.rasys@gmail.com>
2018-08-03 23:55:28 -0400

    Various random updates to the test script

--- backend/test.php
+++ backend/test.php
@@ -1,12 +1,14 @@
 <?php
+$autoloader = require '../vendor/autoload.php';
 
-include 'core/classes/DataContainer.class.php';
-include 'core/classes/Datum.class.php';
+use Core\DataContainer;
 
 $data = new DataContainer(
 [
     'Name' => 'Steve',
     'Age' => 36,
+    'Fraction' => 3.14159,
+    'Alive' => true,
     'Address' => [
         'Street' => '123 Fake Street',
         'State' => 'Vermont'
@@ -29,20 +31,41 @@ echo "\nget\n";
 // var_dump($data->getAge());
 
 var_dump($data->Name);
+var_dump($data->Alive);
 var_dump($data->Age);
-var_dump($data->Address->Street);
+var_dump($data->Fraction);
 
-echo "\nset again\n";
-// var_dump($data->SetName('Doug'));
-var_dump($data->Name = 'Doug');
-var_dump($data->Address->Street = '197 Not Real Blvd.');
+// var_dump($data->Name());
 
-echo "\nget again\n";
-var_dump($data->Name);
-var_dump($data->Address->Street);
+// var_dump($data->Name);
+// var_dump($data->Age);
+// var_dump($data->Address->Street);
 
 // echo "\nupdate type\n";
-// var_dump($data->Name()->setType('string'));
+// // var_dump($data->Name()->setType('string'));
+// var_dump($data->Alive()->setType('boolean'));
+
+echo "\nset again\n";
+// var_dump($data->SaveName('Doug'));
+var_dump($data->Name = 45);
+var_dump($data->Alive = 'beans');
+var_dump($data->Age = 'donut');
+var_dump($data->Fraction = false);
+
+// var_dump($data->Name = 'Doug');
+// var_dump($data->Address->Street = '197 Not Real Blvd.');
+
+echo "\nget again\n";
+// var_dump($data->getName());
+// var_dump($data->getAge());
+// var_dump($data->Alive);
+
+var_dump($data->Name);
+var_dump($data->Alive);
+var_dump($data->Age);
+var_dump($data->Fraction);
+
+// var_dump($data->Address->Street);
 
 // echo "\nobject\n";
 // var_dump($data);
