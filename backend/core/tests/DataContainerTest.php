814000f3d3bc7ea47d78f958227f7563553f31c7
Chris Rasys <chris.rasys@gmail.com>
2018-08-04 00:55:24 -0400

    Setting up initial unit testing for DataContainer class
    #3

---
+++ backend/core/tests/DataContainerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace CoreTests;
+
+use PHPUnit\Framework\TestCase;
+
+use Core\DataContainer;
+use Core\Datum;
+
+final class DataContainerTest extends TestCase {
+    private $testDataSimple = [
+        'Name' => "John",
+    ];
+
+    private $testDataComplex = [
+        'Name' => 'Steve',
+        'Age' => 36,
+        'Fraction' => 3.14159,
+        'Alive' => true,
+        'Address' => [
+                'Street' => '123 Fake Street',
+                'State' => 'Vermont'
+            ]
+        ];
+
+    public function testCanBeCreatedFromSimpleArray(): void
+    {
+        $dataContainer = new DataContainer($this->testDataSimple);
+
+        $this->assertInstanceOf(
+            DataContainer::class,
+            $dataContainer
+        );
+    }
+
+    public function testCanBeCreatedFromComplexArray(): void
+    {
+        $dataContainer = new DataContainer($this->testDataComplex);
+
+        $this->assertInstanceOf(
+            DataContainer::class,
+            $dataContainer
+        );
+    }
+
+    public function testCanGetValueByMethodCall(): Void
+    {
+        $dataContainer = new DataContainer($this->testDataComplex);
+
+        $this->assertEquals('Steve', $dataContainer->getName());
+    }
+
+    public function testCanGetValueByDirectAccess(): Void
+    {
+        $dataContainer = new DataContainer($this->testDataComplex);
+
+        $this->assertEquals('Steve', $dataContainer->Name);
+    }
+
+    public function testCanGetValueFromSubObject(): Void
+    {
+        $dataContainer = new DataContainer($this->testDataComplex);
+
+        $this->assertEquals('Vermont', $dataContainer->Address->State);
+        $this->assertEquals('Vermont', $dataContainer->getAddress()->getState());
+    }
+
+    public function testCanSetValueByMethodCall(): Void
+    {
+        $dataContainer = new DataContainer($this->testDataComplex);
+
+        $dataContainer->setName('Doug');
+
+        $this->assertEquals('Doug', $dataContainer->Name);
+    }
+
+    public function testCanSetValueByDirectAccess(): Void
+    {
+        $dataContainer = new DataContainer($this->testDataComplex);
+
+        $dataContainer->Name = 'Doug';
+
+        $this->assertEquals('Doug', $dataContainer->Name);
+    }
+}
